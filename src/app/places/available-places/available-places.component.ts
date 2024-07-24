ffb54624724627fc40c03fadcd309ae27ee34ecd
saurabprasher <saurab015@gmail.com>
2024-07-24 12:25:58 -0400

    handling errors

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/core';
 import { Place } from '../place.model';
 import { HttpClient } from '@angular/common/http';
-import { map, Subscription } from 'rxjs';
+import { catchError, map, Subscription, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -20,6 +20,7 @@ export class AvailablePlacesComponent implements OnInit, OnDestroy {
   places: Place[] = [];
 
   isFetching = false;
+  error = '';
 
   // private destroyRef = inject(DestroyRef);
   private subscription!: Subscription;
@@ -33,11 +34,21 @@ export class AvailablePlacesComponent implements OnInit, OnDestroy {
       }>('http://localhost:3000/places', {
         // observe:'events',
       })
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) =>
+          throwError(() => new Error('Something went wrong'))
+        )
+      )
+
       .subscribe({
         next: (places) => {
           console.log(places);
           this.places = places;
+        },
+        error: (error: Error) => {
+          this.error = error.message;
+          this.isFetching = false;
         },
         complete: () => (this.isFetching = false),
       });
