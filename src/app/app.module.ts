86fb90d97dfe8327925e76379f30e807f76bbb4e
saurabprasher <saurab015@gmail.com>
2024-07-24 17:21:30 -0400

    interceptors added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,42 @@ import { AppComponent } from './app.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { AvailablePlacesModule } from './places/available-places/available-places.module';
 import { UserPlacesModule } from './places/user-places/user-places.module';
 import { PlacesContainerModule } from './places/places-container/places-container.module';
 import { PlacesModule } from './places/places.module';
 import { ErrorModalComponent } from '../shared/modal/error-modal/error-modal.component';
+import { tap } from 'rxjs';
+
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  // const req = request.clone({
+  //   headers: request.headers.set('X-DEBUG', 'TESTING'),
+  // });
+  // console.log('Outgoing request: ');
+  // console.log(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Incoming response: ');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,7 +51,7 @@ import { ErrorModalComponent } from '../shared/modal/error-modal/error-modal.com
     PlacesModule,
     ErrorModalComponent,
   ],
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
